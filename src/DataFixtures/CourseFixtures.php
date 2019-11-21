c34fef16460c4acd6cba8d17f1699657b771bfe2
Jack Bremner <jack@banntech.co.uk>
2019-11-21 07:26:33 +0000

    add course and coursework fixtures

--- src/DataFixtures/CourseFixtures.php
+++ src/DataFixtures/CourseFixtures.php
@@ -2,22 +2,24 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Student;
+use App\Entity\Course;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
 
-class StudentFixture extends Fixture
+class CourseFixtures extends Fixture
 {
+    public const COURSE_COURSEWORK_REFERENCE = 'admin-user';
+
     protected $faker;
 
     public function load(ObjectManager $manager)
     {
         $this->faker = Factory::create();
         for ($i = 1; $i <= 100; $i++) {
-            $student = new Student();
-            $student->setName($this->faker->name);
-            $manager->persist($student);
+            $course = new Course();
+            $course->setName($this->faker->words(3, true));
+            $manager->persist($course);
         }
 
         $manager->flush();
