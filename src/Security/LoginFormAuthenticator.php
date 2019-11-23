baeb35569903da540b6c2a7cb52b5b2cb140e907
Jack Bremner <jack@banntech.co.uk>
2019-11-23 18:01:25 +0000

    add Home controller, webpack encore, yarn, Boostrap (& deps), and add Boostrap to forms/base.twig

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -86,7 +86,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate('course_index'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
