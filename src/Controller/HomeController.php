d3561de47128106ab36be3502126378532c94c6f
Jack Bremner <jack@banntech.co.uk>
2019-11-25 09:22:53 +0000

    add margin to header, format forms, add auth to home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,6 +10,7 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
+     * @IsGranted("ROLE_USER")
      */
     public function index()
     {
